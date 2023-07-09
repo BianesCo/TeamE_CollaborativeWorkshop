2e2ba8e1510305ee42b461c2cee3a437db23db89
Eimyricaurte <eimy.ricaurte01@uptc.edu.co>
2023-07-08 21:40:43 -0500

    add project

--- src/interfaces/MagicNum.java
+++ src/interfaces/MagicNum.java
@@ -1,10 +1,35 @@
 package interfaces;
-
+import java.util.Arrays;
+import javax.swing.JOptionPane;
 public class MagicNum {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Enter a numer"));
+        boolean isMagic = n(number);
+        JOptionPane.showMessageDialog(null, isMagic); 
+    }
+    
+    public static boolean n(int number) {
+        String num= Integer.toString(number);
+        
+        StringBuilder inv = new StringBuilder(num);
+        inv.reverse();
+        
+        char[] des = num.toCharArray();
+        Arrays.sort(des);
+        StringBuilder desord= new StringBuilder();
+        for (int i = des.length - 1; i >= 0; i--) {
+            desord.append(des[i]);
+        }
+        
+        char[] asc = num.toCharArray();
+        Arrays.sort(asc);
+        
+        int result = Integer.parseInt(inv.toString()) - Integer.parseInt(new String(desord));
+        
+        if (result == number) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
