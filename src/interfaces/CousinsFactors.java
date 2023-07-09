bb31622989e841196446cf201cab1f4884a2807a
BianesCo <bibian.corredor@gmail.com>
2023-07-09 15:18:34 -0500

    agregar factores primos a menu

--- src/interfaces/CousinsFactors.java
+++ src/interfaces/CousinsFactors.java
@@ -1,39 +1,9 @@
 package interfaces;
 
-import javax.swing.JOptionPane;
 
 public class CousinsFactors {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          try {
-	       cousinsFact();
-	       
-		  } catch (NumberFormatException e) {
-			// TODO: handle exception
-			JOptionPane.showMessageDialog(null,"----- ERROR -----");
-		  }
-		
-		
-	}
-
-	private static void cousinsFact() {
-		// TODO Auto-generated method stub
-		String number = JOptionPane.showInputDialog(" Enter Number");
-		int num = Integer.parseInt(number);
-		if (num>1) {
-			if (num>1 && num<4) {
-				JOptionPane.showMessageDialog(null,number + "=" + number );
-			}
-			JOptionPane.showMessageDialog(null,number + "=" + decomposeCoisins(num) );
-			
-		}else {
-			JOptionPane.showMessageDialog(null, " --- ERROR---");
-		}
-		
-	}
-
-	private static String decomposeCoisins(int number) {
+	public static String decomposeCoisins(int number) {
 		StringBuilder exponent = new StringBuilder();
 		
 		for (int factors = 2; factors <=number; factors++) {
--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,10 +1,71 @@
 package main;
 
+import javax.swing.JOptionPane;
+
+import interfaces.CousinsFactors;
+
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		mainMenu();
 
+	}
+
+	private static void mainMenu() {
+		String menu = " ------- MAIN MENU ------ \n\n" +
+	     "1. ROMAN NUMERALS \n"+
+		 "2. PRIME FACTORS \n"+
+	     "3. DELETE SPACES \n"+
+		 "4. EGOTISTICAL NUMBERS \n"+
+	     "5. MAGIC NUMBERS \n"+
+		 "6. DATES \n"+
+	     "Y. EXIT THE PROGRAM \n"+
+		 "-- WRITE YOUR CHOICE -- \n";
+	char option = 0;
+	do {
+		option = Character.toUpperCase(JOptionPane.showInputDialog(menu).charAt(0));
+		 switch (option) {
+		case '1': 
+			break;
+		case '2':
+			try {
+			       cousinsFact();
+			       
+				  } catch (NumberFormatException e) {
+					// TODO: handle exception
+					JOptionPane.showMessageDialog(null,"----- ERROR -----");
+				  }
+			break;
+		case '3':
+			break;
+		case '4':
+			break;
+		case '5':
+			break;
+		case '6':
+			break;
+		case 'Y':
+		}
+	} while (true);
+		
+	}
+
+	private static void cousinsFact() {
+		// TODO Auto-generated method stub
+		String number = JOptionPane.showInputDialog(" Enter Number");
+		int num = Integer.parseInt(number);
+		if (num>1) {
+			if (num>1 && num<4) {
+				JOptionPane.showMessageDialog(null,number + "=" + number );
+			}
+			JOptionPane.showMessageDialog(null,number + "=" + CousinsFactors.decomposeCoisins(num) );
+			
+		}else {
+			JOptionPane.showMessageDialog(null, " --- ERROR---");
+		}
+		
+		
 	}
 
 }
